a65450155cbb98e8fd4aa8b6907f51f8c1f3f304
Nate Mallison <healthynate@gmail.com>
2018-04-16 14:42:02 -0400

    updated index.html to show example usage, flushed out json response

--- timestampMicroservice/app.js
+++ timestampMicroservice/app.js
@@ -11,6 +11,29 @@ app.get('/', (req, res, next) => {
   return res.render('index');
 });
 
+app.get('/:myDate', (req, res, next) => {
+  const myDate = req.params.myDate;
+  let dates = {
+    unixDate: null,
+    naturalDate: null
+  }
+
+  if (myDate.match(/[a-z]/gi)) {
+    dates.naturalDate = new Date(myDate.replace('%20', ''));
+    dates.unixDate = Math.floor(new Date(dates.naturalDate).getTime() / 1000);
+  } else {
+    dates.naturalDate = new Date(myDate * 1000);
+    dates.unixDate = myDate;
+  }
+
+  if (dates.naturalDate === 'Invalid date') {
+    dates.unixDate = null;
+    dates.naturalDate = null;
+  }
+
+  return res.json(dates);
+})
+
 // catch 404 and send to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
