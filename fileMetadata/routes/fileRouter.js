72dd0ef401a164a1dbeebbc79894c689ae68fdaa
Nate Mallison <healthynate@gmail.com>
2018-04-19 13:21:08 -0400

    added upload route and output to JSON results, added file upload in html

--- fileMetadata/routes/fileRouter.js
+++ fileMetadata/routes/fileRouter.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const multer = require('multer');
+const upload = multer();
 const router = express.Router();
 
 router
-.route('/')
-.get((req, res, next) => {
-  return res.render('index');
-});
+  .route('/')
+  .get((req, res, next) => {
+    return res.render('index');
+  });
+
+router
+  .route('/getMetadata')
+  .post(upload.single('fileUpload'), (req, res, next) => {
+    const fileMetadata = {
+      originalname: req.file.originalname,
+      encoding: req.file.encoding,
+      mimetype: req.file.mimetype,
+      size: req.file.size
+    }
+    return res.send(fileMetadata);
+  })
 
   module.exports = router;
