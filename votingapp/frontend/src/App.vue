9f8049f238d9b6d45ed1045ebec041cb63201388
Nate Mallison <healthynate@gmail.com>
2018-05-24 12:14:55 -0400

    basic front end app started

---
+++ votingapp/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  components: {
+    'app-header': Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ votingapp/frontend/src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mt-2 border rounded">
+    <h2 class="navbar-brand noPointer">Voting App</h2>
+    <ul class="navbar-nav d-flex w-100">
+      <router-link to="/" tag="li" active-class="active" exact class="nav-item">
+        <a class="nav-link">Home</a>
+      </router-link>
+      <router-link to="/signup" tag="li" active-class="active" class="nav-item">
+        <a class="nav-link">Sign Up</a>
+      </router-link>
+      <router-link to="/signin" tag="li" active-class="active" class="nav-item">
+        <a class="nav-link">Sign In</a>
+      </router-link>
+      <router-link to="/polls" tag="li" active-class="active" class="nav-item">
+        <a class="nav-link">See Polls</a>
+      </router-link>
+      <a class="nav-link ml-auto" href="https://www.natethedev.com/" target="_blank" rel="noopener">Created by Nate Mallison</a>
+    </ul>
+  </nav> 
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+.nav-link {
+  cursor: pointer;
+}
+
+.noPointer {
+  cursor: default !important;
+}
+
+.active {
+  font-weight: bold;
+}
+</style>
