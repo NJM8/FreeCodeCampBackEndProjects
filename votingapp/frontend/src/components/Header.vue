4902adc11c262141d8caa6e8c8d023e6dac816ec
Nate Mallison <healthynate@gmail.com>
2018-05-25 09:22:35 -0400

    added dropdown collapse to header

--- votingapp/frontend/src/components/Header.vue
+++ votingapp/frontend/src/components/Header.vue
@@ -1,30 +1,32 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light mt-2 border rounded">
-    <h2 class="navbar-brand noPointer">Voting App</h2>
-    <ul class="navbar-nav d-flex w-100">
-      <router-link to="/home" tag="li" active-class="active" exact class="nav-item">
-        <a class="nav-link">Home</a>
-      </router-link>
-      <router-link to="/signup" tag="li" active-class="active" class="nav-item">
-        <a class="nav-link">Sign Up</a>
-      </router-link>
-      <router-link to="/signin" tag="li" active-class="active" class="nav-item">
-        <a class="nav-link">Sign In</a>
-      </router-link>
-      <router-link to="/polls" tag="li" active-class="active" class="nav-item">
-        <a class="nav-link">See Polls</a>
-      </router-link>
-      <router-link v-if="auth" to="/user" tag="li" active-class="active" class="nav-item">
-        <a class="nav-link">User Page</a>
-      </router-link>
-      <router-link v-if="auth" to="/userPolls" tag="li" active-class="active" class="nav-item">
-        <a class="nav-link">User Polls</a>
-      </router-link>
-      <li v-if="auth">
-        <button class="logout" @click="onLogout">Log Out</button>
-      </li>
-      <a class="nav-link ml-auto" href="https://www.natethedev.com/" target="_blank" rel="noopener">Created by Nate Mallison</a>
-    </ul>
+    <h2 class="navbar-brand">Voting App</h2>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav w-100 d-flex">
+        <router-link to="/home" tag="li" active-class="active" exact class="nav-item">
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link to="/signup" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">Sign Up</a>
+        </router-link>
+        <router-link to="/signin" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">Sign In</a>
+        </router-link>
+        <router-link to="/polls" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">See Polls</a>
+        </router-link>
+        <router-link v-if="isAuth" to="/userPage" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">{{ userName }}'s Page</a>
+        </router-link>
+        <li v-if="isAuth">
+          <button class="logout" @click="onLogout">Log Out</button>
+        </li>
+        <a class="nav-link ml-auto" href="https://www.natethedev.com/" target="_blank" rel="noopener">Created by Nate Mallison</a>
+      </ul>
+    </div>
   </nav> 
 </template>
 
@@ -32,8 +34,11 @@
 
 export default {
   computed: {
-    auth(){
+    isAuth(){
       return this.$store.getters.isAuth;
+    },
+    userName(){
+      return this.$store.getters.userName;
     }
   },
   methods: {
@@ -47,10 +52,6 @@ export default {
 <style scoped>
 .nav-link {
   cursor: pointer;
-}
-
-.noPointer {
-  cursor: default !important;
 }
 
 .active {
