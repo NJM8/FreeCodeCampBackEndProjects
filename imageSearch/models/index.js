6b0ebbff76bf3532fc8ffd1bb7903ae752d52c3e
natemallison <healthynate@gmail.com>
2020-10-05 09:40:48 -0400

    Mongoose updates.

--- imageSearch/models/index.js
+++ imageSearch/models/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.set("debug", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(process.env.DB_URI || "mongodb://localhost/imageSearch")
+  .connect(process.env.DB_URI || "mongodb://localhost/imageSearch", {
+    useNewUrlParser: true,
+  })
   .then(() => {
     return console.log("Connected to Mongo DB");
   })
